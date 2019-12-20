e3b4c2e02d541b378ed8525a34a73f34921ca7bf
dyf901 <1378550520@qq.com>
2019-12-20 17:50:46 +0800

    2019/12/20  17:50  智慧工地代码备份

---
+++ src/main/java/com/zty/project/dao/CommodityDao.java
@@ -0,0 +1,25 @@
+package com.zty.project.dao;
+
+import com.zty.project.entity.Commodity;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CommodityDao {
+    //分页查找所有商品信息
+    List<Commodity> find_commodity(Map map);
+
+    //统计总数
+    long total();
+
+    //增加商品
+    int add_commodity(Map map);
+
+    //修改商品信息 状态改为0
+    int update_commodity_active0(Map map);
+
+    //修改商品信息 状态改为1
+    int update_commodity_active1(Map map);
+
+    //
+}
